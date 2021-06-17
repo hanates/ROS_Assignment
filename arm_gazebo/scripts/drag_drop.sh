96732b5cd9683deca0e7c25c13505296f1d8f211
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2021-06-17 04:23:35 +0300

    Removed comments from shell script

--- arm_gazebo/scripts/drag_drop.sh
+++ arm_gazebo/scripts/drag_drop.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 sleep 3
-#  Pose: -0.179457 -3.225999 0.78532
-# rostopic pub --once /ik_angles arm_lib/actuator_pos -- {3,1,0} 
-# rostopic pub --once /control arm_lib/control_cmd catch
-# rostopic pub --once /ik_angles arm_lib/actuator_pos -- {2.5,0,1.5}
-# rostopic pub --once /ik_angles arm_lib/actuator_pos -- {2,0,0.5}
-# rostopic pub --once /control arm_lib/control_cmd release
-# rostopic pub --once /ik_angles arm_lib/actuator_pos -- {2.5,0,0}
 rostopic pub --once /ik_angles arm_lib/actuator_pos -- {2.3,0.28,0.25} 
 rostopic pub --once /control arm_lib/control_cmd catch
 rostopic pub --once /ik_angles arm_lib/actuator_pos -- {2,-1.3,1.5}
